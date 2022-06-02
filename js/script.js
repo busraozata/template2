21af0e6d3ceb80682a4e67ddc14ac44ebf6ffb2b
busraozata <busraozata1998@gmail.com>
2022-06-02 18:29:56 +0300

    first commit thuesday

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-$(".twentytwenty-container").twentytwenty({default_offset_pct: 0.5});
+$(".twentytwenty-container").twentytwenty({ default_offset_pct: 0.5 });
 
 
 const hamburgerMenu = document.querySelector('.hamburger');
@@ -21,10 +21,12 @@ var swiper = new Swiper(".mySwiper", {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
     },
+
     autoplay: {
         delay: 2500,
         disableOnInteraction: false,
     },
+
     // Enable debugger
     debugger: true,
 
@@ -106,6 +108,10 @@ var swiper = new Swiper(".mySwiperDoctors", {
             spaceBetween: 40,
         },
         1024: {
+            slidesPerView: 2,
+            spaceBetween: 50,
+        },
+        1200: {
             slidesPerView: 3,
             spaceBetween: 50,
         },
@@ -118,32 +124,32 @@ var swiper = new Swiper(".mySwiperLastPosts", {
     spaceBetween: 5,
     loop: true,
     autoplay: {
-      delay: 1500,
+        delay: 1500,
     },
     breakpoints: {
-      640: {
-        slidesPerView: 3,
-        spaceBetween: 5,
-      },
-      768: {
-        slidesPerView: 3,
-        spaceBetween: 50,
-      },
-      1024: {
-        slidesPerView: 3,
-        spaceBetween: 10,
-      },
+        640: {
+            slidesPerView: 3,
+            spaceBetween: 5,
+        },
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 50,
+        },
+        1024: {
+            slidesPerView: 3,
+            spaceBetween: 10,
+        },
     },
-  });
+});
 
-/* $(function () {
+$(function () {
 
     $('#datetimepicker2').datetimepicker({
         format: 'DD-MM-YYYY'
     });
 
 });
- */
+
 const acc = document.getElementsByClassName("accordion");
 let i;
 
